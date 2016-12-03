1d6510637e79a5c6f2a13bf5faf2bb7285dfc915
Jay Vana <jsvana@mtu.edu>
2016-12-02 21:33:49 -0800

    Add day 3

---
+++ day_3.cpp
@@ -0,0 +1,43 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+const bool valid_triangle(const int s1, const int s2, const int s3) {
+  if (s1 + s2 <= s3) {
+    return false;
+  } else if (s1 + s3 <= s2) {
+    return false;
+  } else if (s2 + s3 <= s1) {
+    return false;
+  }
+  return true;
+}
+
+const int count_valid_triangles(const std::string& path) {
+  std::ifstream triangle_f(path);
+
+  if (!triangle_f.is_open()) {
+    std::cerr << "Unable to open triangles file \"" << path << "\"" << std::endl;
+    return 0;
+  }
+
+  int valid = 0;
+  std::string line;
+  while (std::getline(triangle_f, line)) {
+    std::stringstream line_s{line};
+    int s1, s2, s3;
+    line_s >> s1;
+    line_s >> s2;
+    line_s >> s3;
+
+    if (valid_triangle(s1, s2, s3)) {
+      valid++;
+    }
+  }
+
+  return valid;
+}
+
+int main(int, char**) {
+  std::cout << count_valid_triangles("day_3.txt") << " valid triangles" << std::endl;
+}
